666295510b2b3f2d3558983e66deffbf109186ed
Carlos <93174.joan23@fje.edu>
2018-10-30 18:38:11 +0100

    para marc con amor

---
+++ eliminar_reserva.proc.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+	$cadena=$_SESSION["usuario"];
+	$link = mysqli_connect('localhost', 'root', '', '1819_exemple');
+			
+			if (!$link) {
+			    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+			    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+			    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+			    exit;
+			}
+	$query = mysqli_query($link, "SELECT * from tbl_reserva where nusuario_empleado='$cadena'");
+	if($row['nusuario_empleado'] = mysqli_fetch_array($query)){
+	mysqli_query($link, "DELETE FROM tbl_categoria WHERE nombre_recurso=$_REQUEST[nombre_recurso]");
+	header("Location: formulario_recursos.php");
+	}else{
+	echo 'no tienes permiso para eliminar esta reserva';
+	header("Location: formulario_recursos.php");
+	}
+?>
