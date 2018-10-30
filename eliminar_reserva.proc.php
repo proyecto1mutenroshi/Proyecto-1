21e4b30d9909683c052cc1123a7bebf2ca2e24d5
Carlos <93174.joan23@fje.edu>
2018-10-30 19:45:51 +0100

    Mejora de codigo 1

--- eliminar_reserva.proc.php
+++ eliminar_reserva.proc.php
@@ -9,8 +9,8 @@
 			    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
 			    exit;
 			}
-	$query = mysqli_query($link, "SELECT * from tbl_reserva where nusuario_empleado='$cadena'");
-	if($row['nusuario_empleado'] = mysqli_fetch_array($query)){
+	$query = mysqli_query($link, "SELECT * from tbl_reserva where id_empleado='$_SESSION[idusuario]'");
+	if($row['id_empleado'] = mysqli_fetch_array($query)){
 	mysqli_query($link, "DELETE FROM tbl_categoria WHERE nombre_recurso=$_REQUEST[nombre_recurso]");
 	header("Location: formulario_recursos.php");
 	}else{
